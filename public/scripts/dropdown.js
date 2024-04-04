635ea4dd2eeadf98cbea9f29eb1ee7d8840c14d5
Diana Moretti <morettidn@gmail.com>
2024-04-04 17:51:21 +0200

    adding styles

--- public/scripts/dropdown.js
+++
@@ -1,8 +0,0 @@
-let dropdown__button = document.getElementsByClassName("dropdown__button");
-
-for (let i = 0; i < dropdown__button.length; i++) {
-  dropdown__button[i].addEventListener("click", function () {
-    let dropdown = this.nextElementSibling;
-    dropdown.classList.toggle("active");
-  });
-}
